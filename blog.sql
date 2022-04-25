950e1167edc923556f6c84d15f3caff61cf5f3c6
DOMESDAY <79838340+DOMESDAY7@users.noreply.github.com>
2022-04-26 00:51:17 +0200

    feat(bdd)

--- blog.sql
+++ blog.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : dim. 17 avr. 2022 à 20:37
+-- Généré le : mar. 26 avr. 2022 à 00:50
 -- Version du serveur : 10.4.22-MariaDB
 -- Version de PHP : 8.1.1
 
@@ -43,7 +43,7 @@ CREATE TABLE `article` (
 --
 
 INSERT INTO `article` (`id`, `texte`, `datepost`, `titre`, `miniature`, `img1`, `img2`, `categorie_id`) VALUES
-(1, 'test', '2022-03-21', 'test', '', '', '', 1);
+(1, 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos non nulla voluptas ducimus porro omnis facilis quasi! Temporibus quos commodi hic voluptas, consequuntur, magni dolore inventore unde nemo alias fugit.\nIpsum illo, ipsam eligendi obcaecati in doloribus cumque asperiores, possimus, non reprehenderit laudantium voluptatem repellendus dicta consectetur suscipit quidem totam molestiae quisquam itaque a! Id facere ratione voluptate adipisci alias!\nMaiores quos expedita sapiente architecto eos dolorum quas quae officia assumenda repellat! Mollitia, itaque illo doloribus repellendus rem modi repellat omnis, labore libero, porro voluptates fugiat veritatis accusantium officia ratione.\nPariatur vero magni nesciunt modi laudantium. Aliquid expedita quod, possimus quae reiciendis debitis vero eum vel voluptates nihil molestiae repellat veritatis saepe fugiat beatae laboriosam illum quas eligendi aspernatur quo.\nDeserunt, exercitationem totam? Molestias soluta, delectus, quos vel ea sapiente, incidunt amet aut earum architecto esse deleniti nihil voluptate. Aliquid commodi at atque ab nesciunt officia odio sunt sint asperiores!', '2022-03-21', 'Lorem', '', '', '', 1);
 
 -- --------------------------------------------------------
 
@@ -52,17 +52,17 @@ INSERT INTO `article` (`id`, `texte`, `datepost`, `titre`, `miniature`, `img1`, 
 --
 
 CREATE TABLE `categorie` (
-  `id_categorie` int(11) NOT NULL,
-  `categorie_nom` varchar(50) COLLATE utf8_unicode_ci NOT NULL
+  `categorie_nom` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Déchargement des données de la table `categorie`
 --
 
-INSERT INTO `categorie` (`id_categorie`, `categorie_nom`) VALUES
-(1, 'drama'),
-(2, 'livre');
+INSERT INTO `categorie` (`categorie_nom`, `id`) VALUES
+('drama', 1),
+('livre', 2);
 
 -- --------------------------------------------------------
 
@@ -91,7 +91,11 @@ INSERT INTO `commentaire` (`id_commentaire`, `contenu`, `dateTime`, `utilisateur
 (6, 'test', '2022-04-10', NULL, NULL),
 (7, 'test', '2022-04-10', NULL, NULL),
 (9, 'test', '2022-04-10', 2, 1),
-(10, 'test', '2022-04-17', 2, 1);
+(10, 'test', '2022-04-17', 2, 1),
+(11, 'test', '2022-04-22', 2, 1),
+(12, 'test', '2022-04-22', 2, 1),
+(13, 'mathieu', '2022-04-22', 2, 1),
+(14, 'test', '2022-04-22', 2, 1);
 
 -- --------------------------------------------------------
 
@@ -100,9 +104,28 @@ INSERT INTO `commentaire` (`id_commentaire`, `contenu`, `dateTime`, `utilisateur
 --
 
 CREATE TABLE `souscategorie` (
+  `id` int(11) NOT NULL,
   `sous_categorie` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `id_sous_categorie` int(11) NOT NULL
+  `categorie_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Déchargement des données de la table `souscategorie`
+--
+
+INSERT INTO `souscategorie` (`id`, `sous_categorie`, `categorie_id`) VALUES
+(1, 'romance', 1),
+(2, 'sf et fantaisie', 1),
+(3, 'BL / GL', 1),
+(4, 'sport', 1),
+(5, 'chine', 1),
+(6, 'romance', 2),
+(7, 'sf et fantaisie', 2),
+(8, 'adolescent', 2),
+(9, 'manga', 2),
+(10, 'corée', 1),
+(11, 'thailande', 1),
+(12, 'taiwan', 1);
 
 -- --------------------------------------------------------
 
@@ -152,7 +175,7 @@ ALTER TABLE `article`
 -- Index pour la table `categorie`
 --
 ALTER TABLE `categorie`
-  ADD PRIMARY KEY (`id_categorie`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Index pour la table `commentaire`
@@ -166,7 +189,8 @@ ALTER TABLE `commentaire`
 -- Index pour la table `souscategorie`
 --
 ALTER TABLE `souscategorie`
-  ADD PRIMARY KEY (`id_sous_categorie`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_5E7C6E57BCF5E72D` (`categorie_id`);
 
 --
 -- Index pour la table `utilisateur`
@@ -188,19 +212,19 @@ ALTER TABLE `article`
 -- AUTO_INCREMENT pour la table `categorie`
 --
 ALTER TABLE `categorie`
-  MODIFY `id_categorie` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT pour la table `commentaire`
 --
 ALTER TABLE `commentaire`
-  MODIFY `id_commentaire` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id_commentaire` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT pour la table `souscategorie`
 --
 ALTER TABLE `souscategorie`
-  MODIFY `id_sous_categorie` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT pour la table `utilisateur`
@@ -224,6 +248,12 @@ ALTER TABLE `article`
 ALTER TABLE `commentaire`
   ADD CONSTRAINT `FK_E16CE76B7294869C` FOREIGN KEY (`article_id`) REFERENCES `article` (`id`),
   ADD CONSTRAINT `FK_E16CE76BFB88E14F` FOREIGN KEY (`utilisateur_id`) REFERENCES `utilisateur` (`id`);
+
+--
+-- Contraintes pour la table `souscategorie`
+--
+ALTER TABLE `souscategorie`
+  ADD CONSTRAINT `FK_5E7C6E57BCF5E72D` FOREIGN KEY (`categorie_id`) REFERENCES `categorie` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
